255add09ebc5816c54d90369ba53b20197922ebb
Alexander Rubia <arubiacortes@gmail.com>
2016-03-12 10:46:16 +0100

    fix

--- recipes/opsworks_replicaset.rb
+++ recipes/opsworks_replicaset.rb
@@ -2,7 +2,7 @@
 node.set['mongodb']['is_replicaset'] = false
 node.set['mongodb']['cluster_name'] = node['mongodb']['cluster_name']
 
-include_recipe "mongodb"
+include_recipe "mongodb::install"
 include_recipe 'mongodb::mongo_gem'
 
 Chef::Log.info "Configuring replicaset with OPSWORKS REPLICASET"
