5ab63b62050118beb80ddd113e2fd03211bf5aea
Alexander Rubia <arubiacortes@gmail.com>
2016-03-12 10:35:23 +0100

    fixing bugs

--- recipes/opsworks_replicaset.rb
+++ recipes/opsworks_replicaset.rb
@@ -5,8 +5,6 @@ node.set['mongodb']['cluster_name'] = node['mongodb']['cluster_name']
 include_recipe "mongodb"
 include_recipe 'mongodb::mongo_gem'
 
-::Chef::Recipe.send(:include, MongoDB::OpsWorksHelper)
-
 Chef::Log.info "Configuring replicaset with OPSWORKS REPLICASET"
 
 unless node['mongodb']['is_shard']
@@ -14,5 +12,7 @@ unless node['mongodb']['is_shard']
   replicaset_layer_slug_name = node['opsworks']['instance']['layers'].first
   replicaset_layer_instances = node['opsworks']['layers'][replicaset_layer_slug_name]['instances']
 
-  Chef::ResourceDefinitionList::MongoDB.configure_replicaset(node, replicaset_layer_slug_name, replicaset_members(node, replicaset_layer_instances))
+  replicaset_members = Chef::ResourceDefinitionList::OpsWorksHelper.replicaset_members(node, replicaset_layer_instances)
+
+  Chef::ResourceDefinitionList::MongoDB.configure_replicaset(node, replicaset_layer_slug_name, replicaset_members)
 end
