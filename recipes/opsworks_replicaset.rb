120632032fb7f20c49866542877ef5cf4d4a1292
Alexander Rubia <arubiacortes@gmail.com>
2016-03-12 11:22:56 +0100

    fix bug

--- recipes/opsworks_replicaset.rb
+++ recipes/opsworks_replicaset.rb
@@ -2,6 +2,11 @@
 node.set['mongodb']['is_replicaset'] = false
 node.set['mongodb']['cluster_name'] = node['mongodb']['cluster_name']
 
+default['mongodb']['ruby_gems'] = {
+  :mongo => nil,
+  :bson_ext => nil
+}
+
 include_recipe "mongodb::install"
 include_recipe 'mongodb::mongo_gem'
 
